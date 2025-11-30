f5c10d2aeaae9762dfebc0b14f32e876d580ac1d
dianayenkalo <diana.yenkalo.pp.2025@lpnu.ua>
2025-11-30 21:15:40 +0200

    Delete packages directory

--- packages/backend/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { PostsPage } from '@/components/PostsPage';
-
-export default function Home() {
-  return <PostsPage />;
-}
-
--- packages/shared/src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Shared package exports
- * Contains types and utilities used across backend and frontend
- */
-
-export * from './types';
-export * from './utils';
-
